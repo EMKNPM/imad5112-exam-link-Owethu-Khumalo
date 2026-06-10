bca5403d183446682e35d434beb96d8af75c3692
Owethu-Khumalo <ST10513544@myemeris.edu.za>
2026-06-10 10:31:19 +0200

    created splashscreen
    did declarations
    typecasting
    btn click listener

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,16 +1,48 @@
 package com.example.myapplication
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    //Declarations
+    private lateinit var txtTitle: TextView
+    private lateinit var text2: TextView
+    private lateinit var btnProceed : Button
+
+
+
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        //typecasting
+        btnProceed.findViewById(R.id.btnProceed)
+
+        //buttonclick listener
+        btnProceed.setOnClickListener {
+            btnProceed.setOnClickListener {
+                val intent = Intent(this, Mainscreen::class.java)
+                startActivity(intent)
+                finish()
+        }
+
+
+
+
+
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
